5fe0b9b3fadb7c062af5fafbdb111410b4373914
orloveste <orlovevm@yahoo.com>
2020-04-22 15:49:54 +0300

    Book - exception available - print put return
    https://www.hackerrank.com/challenges/30-2d-arrays/tutorial

--- src/main/java/LibraryCatalogue/LibraryCatalogue.java
+++ src/main/java/LibraryCatalogue/LibraryCatalogue.java
@@ -63,15 +63,28 @@ public class LibraryCatalogue {
         int daysLate = currentDay - (book.getDayCheckedOut() + getLengthOfCheckOutPeriod());
         if ( daysLate > 0 ){
             System.out.println("You owe the library $" + (getInitialLateFee()+daysLate*getFeePerDayLate()+
-                    " because your book is " + daysLate + "days overdue."));;
+                    " because your book is " + daysLate + " days overdue."));;
         } else {
             System.out.println("Book Returned. Thank you");;
         }
         book.setIsCheckedOut(false, -1);
     }
-    public void sorryBookAlreadyCheckOut(Book book){}
-
+    public void sorryBookAlreadyCheckOut(Book book){
+        System.out.println("Sorry, " + book.getTitle() + " is already checked out. It should be back on day " +
+                (book.getDayCheckedOut() + getLengthOfCheckOutPeriod()) + ".");;
+    }
+//todo reserve book
     public static void main(String[] args) {
-
+        Map<String, Book> bookCollection = new HashMap<String, Book>();
+        Book harry = new Book("Harry Potter", 454, 32322); //todo isbn num+char
+        bookCollection.put("Harry Potter", harry);
+        LibraryCatalogue lib = new LibraryCatalogue(bookCollection);
+        lib.checkOutBook("Harry Potter");
+        lib.nextDay();
+        lib.nextDay();
+        lib.checkOutBook("Harry Potter");
+        lib.setDay(17);
+        lib.returnBook("Harry Potter");
+        lib.checkOutBook("Harry Potter");
     }
 }
